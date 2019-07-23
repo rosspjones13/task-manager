e02d3694acd8cd6a3bea2975c82bc9490380c53d
Ross Jones <rossmosis.jones@gmail.com>
2019-07-23 14:55:53 -0400

    created the user/task relationship and implemented authentication to all task CRUD routes

--- src/index.js
+++ src/index.js
@@ -13,3 +13,14 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  const user = await User.findById('5d374f361897d053a0019336')
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks)
+}
+
+main()
